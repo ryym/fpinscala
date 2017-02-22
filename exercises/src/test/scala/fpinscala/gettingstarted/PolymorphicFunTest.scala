71a2fda63e892a09f9929d10cca7018c1706d747
ryym <ryym.64@gmail.com>
2017-02-22 17:35:14 +0900

    gettingstarted: curry, uncurry, compose

--- exercises/src/test/scala/fpinscala/gettingstarted/PolymorphicFunTest.scala
+++ exercises/src/test/scala/fpinscala/gettingstarted/PolymorphicFunTest.scala
@@ -1,5 +1,4 @@
 import org.scalatest._
-
 import fpinscala.gettingstarted.PolymorphicFunctions._
 
 class PolymorphicFunTest extends FunSuite with Matchers {
@@ -7,5 +6,21 @@ class PolymorphicFunTest extends FunSuite with Matchers {
     isSorted[Int](Array(1, 2, 3, 4), _ > _) should equal (true)
     isSorted[Int](Array(1, 3, 2, 4), _ > _) should equal (false)
     isSorted[Char](Array('a', 'b', 'c'), _ > _) should equal (true)
+  }
+
+  test("curry curries a function") {
+    def some = (a: Int, b: Int) => a + b
+    curry(some)(2)(4) should equal (6)
+  }
+
+  test("uncurry uncurries a function") {
+    def some = (a: Int) => (b: Int) => a + b
+    uncurry(some)(2, 4) should equal (6)
+  }
+
+  test("compose composes two functions") {
+    def double = (a: Int) => a * 2
+    val quadruple = compose(double, double)
+    quadruple(3) should equal (12)
   }
 }
