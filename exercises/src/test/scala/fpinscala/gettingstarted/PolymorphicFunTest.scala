9df8ce36937453c581f3f4549a5ca649ed0feebf
ryym <ryym.64@gmail.com>
2017-02-22 17:32:08 +0900

    gettingstarted(test): Use more abstract test file name

--- exercises/src/test/scala/fpinscala/gettingstarted/PolymorphicFunTest.scala
+++ exercises/src/test/scala/fpinscala/gettingstarted/PolymorphicFunTest.scala
@@ -1,8 +1,8 @@
 import org.scalatest._
 
-import fpinscala.gettingstarted.PolymorphicFunctions.isSorted
+import fpinscala.gettingstarted.PolymorphicFunctions._
 
-class IsSortedTest extends FunSuite with Matchers {
+class PolymorphicFunTest extends FunSuite with Matchers {
   test("isSorted checks if an array is sorted or not") {
     isSorted[Int](Array(1, 2, 3, 4), _ > _) should equal (true)
     isSorted[Int](Array(1, 3, 2, 4), _ > _) should equal (false)
