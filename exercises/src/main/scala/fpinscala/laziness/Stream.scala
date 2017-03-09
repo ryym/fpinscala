9e92665cae6a83ebee9e36e769e5986bb33196bd
ryym <ryym.64@gmail.com>
2017-03-09 23:16:56 +0900

    laziness(Stream): Simplify take, drop (answer)

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -37,17 +37,14 @@ trait Stream[+A] {
   }
 
   def take(n: Int): Stream[A] = this match {
-    case Cons(h, t) =>
-      if (n == 0) Empty
-      else Cons(h, () => t().take(n - 1))
-    case Empty => this
+    case Cons(h, t) if n > 0 => Cons(h, () => t().take(n - 1))
+    case _ => Empty
   }
 
-  def drop(n: Int): Stream[A] = this match {
-    case Empty => this
-    case Cons(_, t) =>
-      if (n == 0) this
-      else t().drop(n - 1)
+  @annotation.tailrec
+  final def drop(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 0 => t().drop(n - 1)
+    case _ => this
   }
 
   def takeWhile(p: A => Boolean): Stream[A] = ???
