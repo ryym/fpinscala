283cb66b68bc0f9295757b4bc38b7185a211194c
ryym <ryym.64@gmail.com>
2017-04-07 23:49:11 +0900

    state(RNG): nonNegativeInt

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -30,7 +30,10 @@ object RNG {
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (int, nextRng) = rng.nextInt
+    (Math.abs(int), nextRng)
+  }
 
   def double(rng: RNG): (Double, RNG) = ???
 
