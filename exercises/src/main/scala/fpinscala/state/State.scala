bdbc93ae178141ed4135da0488894c35957e6171
ryym <ryym.64@gmail.com>
2017-04-12 22:12:28 +0900

    state(RNG): map, map2 via flatMap

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -104,6 +104,12 @@ object RNG {
       else
         nonNegativeLessThan(n)
     })
+
+  def mapViaFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def map2ViaFlatMap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => flatMap(rb)(b => unit(f(a, b))))
 }
 
 case class State[S,+A](run: S => (A, S)) {
