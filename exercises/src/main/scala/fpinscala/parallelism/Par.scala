dbe962fa4ef79bce725bf13d2448fe7c08c9a5d7
ryym <ryym.64@gmail.com>
2017-04-21 18:02:47 +0900

    parallel(Par): map2 with timeout (answer)

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -23,6 +23,46 @@ object Par {
       val bf = b(es)
       UnitFuture(f(af.get, bf.get)) // This implementation of `map2` does _not_ respect timeouts, and eagerly waits for the returned futures. This means that even if you have passed in "forked" arguments, using this map2 on them will make them wait. It simply passes the `ExecutorService` on to both `Par` values, waits for the results of the Futures `af` and `bf`, applies `f` to them, and wraps them in a `UnitFuture`. In order to respect timeouts, we'd need a new `Future` implementation that records the amount of time spent evaluating `af`, then subtracts that time from the available time allocated for evaluating `bf`.
     }
+
+  def map2WithTimeout[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    es => {
+      val (af, bf) = (a(es), b(es))
+      Map2Future(af, bf, f)
+    }
+
+  case class Map2Future[A, B, C](
+    af: Future[A],
+    bf: Future[B],
+    f: (A, B) => C
+  ) extends Future[C] {
+    private var cache: Option[C] = None
+
+    def isDone = cache.isDefined
+
+    def isCancelled = af.isCancelled || bf.isCancelled
+
+    def cancel(evenIfRunning: Boolean): Boolean =
+      af.cancel(evenIfRunning) || bf.cancel(evenIfRunning)
+
+    def get =
+      compute(Long.MaxValue)
+
+    def get(timeout: Long, units: TimeUnit) =
+      compute(TimeUnit.NANOSECONDS.convert(timeout, units))
+
+    private def compute(timeoutInNanos: Long): C = cache match {
+      case Some(c) => c
+      case None => {
+        val start = System.nanoTime
+        val a = af.get(timeoutInNanos, TimeUnit.NANOSECONDS)
+        val stop = System.nanoTime
+        val b = bf.get(timeoutInNanos - (stop - start), TimeUnit.NANOSECONDS)
+        val c = f(a, b)
+        cache = Some(c)
+        c
+      }
+    }
+  }
   
   def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
     es => es.submit(new Callable[A] { 
